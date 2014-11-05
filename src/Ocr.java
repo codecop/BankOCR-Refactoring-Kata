fbc469cff74631114a10ae68d9697ebc479d0ad0
Jason Felice <jason.m.felice@gmail.com>
2014-11-05 21:38:10 +0100

    Move variable declarations to tightest scope

--- src/Ocr.java
+++ src/Ocr.java
@@ -12,19 +12,16 @@ public class Ocr {
 
     public static List<String> parse(String... lines) {
         final List<String> result = new ArrayList<String>();
-        final char[] work = new char[13];
-
-        int i,j,k,l,m;
-        boolean got1, ok;
-        for (i = 0; i < lines.length; i += 4) {
+        for (int i = 0; i < lines.length; i += 4) {
+            final char[] work = new char[13];
             work[9] = work[10] = work[11] = work[12] = ' ';
-            for (j = 0; j < 9; ++j) {
+            for (int j = 0; j < 9; ++j) {
                 work[j] = '?';
-                got1 = false;
-                for (k = 0; k <= 9; ++k) {
-                    ok = true;
-                    for (l = 0; l < 4; ++l) {
-                        for (m = 0; m < 4; ++m) {
+                boolean got1 = false;
+                for (int k = 0; k <= 9; ++k) {
+                    boolean ok = true;
+                    for (int l = 0; l < 4; ++l) {
+                        for (int m = 0; m < 4; ++m) {
                             if (DIGITS[l][4*k+m] != lines[i+l].charAt(4*j+m))
                                 ok = false;
                         }
