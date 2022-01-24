6f9f8ea0557485b37061d2432e17a82222aeccfd
Peter Kofler <peter.kofler@code-cop.org>
2022-01-24 22:34:01 +0100

    Port OCR test to Cpp.

---
+++ cpp/ocr_gtest.cpp
@@ -0,0 +1,76 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "Ocr.h"
+
+using namespace std;
+
+TEST(OcrTest, parses_one_record_with_888888888)
+{
+    string lines[] = {" _   _   _   _   _   _   _   _   _  ", /* */
+                      "|_| |_| |_| |_| |_| |_| |_| |_| |_| ", /* */
+                      "|_| |_| |_| |_| |_| |_| |_| |_| |_| ", /* */
+                      "                                    "};
+    vector<string> ocr = Ocr::parse(lines);
+    EXPECT_THAT(ocr, testing::ElementsAre("888888888    "));
+}
+
+TEST(OcrTest, parses_two_records_with_888888888)
+{
+    string lines[] = {" _   _   _   _   _   _   _   _   _  ", /* */
+                      "|_| |_| |_| |_| |_| |_| |_| |_| |_| ", /* */
+                      "|_| |_| |_| |_| |_| |_| |_| |_| |_| ", /* */
+                      "                                    ", /* */
+                      " _   _   _   _   _   _   _   _   _  ", /* */
+                      "|_| |_| |_| |_| |_| |_| |_| |_| |_| ", /* */
+                      "|_| |_| |_| |_| |_| |_| |_| |_| |_| ", /* */
+                      "                                    "};
+    vector<string> ocr = Ocr::parse(lines);
+    EXPECT_THAT(ocr, testing::ElementsAre("888888888    ", /* */
+                                          "888888888    "));
+}
+
+TEST(OcrTest, parses_one_record_with_123456790)
+{
+    string lines[] = {"     _   _       _   _   _   _   _  ", /* */
+                      "  |  _|  _| |_| |_  |_    | |_| | | ", /* */
+                      "  | |_   _|   |  _| |_|   |  _| |_| ", /* */
+                      "                                    "};
+    vector<string> ocr = Ocr::parse(lines);
+    EXPECT_THAT(ocr, testing::ElementsAre("123456790    "));
+}
+
+TEST(OcrTest, parses_two_records)
+{
+    string lines[] = {"     _   _       _   _   _   _   _  ", /* */
+                      "  |  _|  _| |_| |_  |_    | |_| | | ", /* */
+                      "  | |_   _|   |  _| |_|   |  _| |_| ", /* */
+                      "                                    ", /* */
+                      " _       _   _   _       _   _   _  ", /* */
+                      "|_| |_|   | |_|   | |_|  _|  _|  _| ", /* */
+                      " _|   |   | |_|   |   | |_  |_  |_  ", /* */
+                      "                                    "};
+    vector<string> ocr = Ocr::parse(lines);
+    EXPECT_THAT(ocr, testing::ElementsAre("123456790    ", /* */
+                                          "947874222    "));
+}
+
+TEST(OcrTest, parses_illegal_digit)
+{
+    string lines[] = {"     _   _       _   _   _   _   _  ", /* */
+                      "  |  _| |_|  _| |_  |_    | |_| | | ", /* */
+                      "  | |_   _    |  _| |_|   |  _| |_| ", /* */
+                      "                                    "};
+    vector<string> ocr = Ocr::parse(lines);
+    EXPECT_THAT(ocr, testing::ElementsAre("12??56790 ILL"));
+}
+
+// TEST(OcrTest, checksum_fail_returns_ERR)
+// {
+//     string lines[] = {" _   _       _   _           _   _  ", /* */
+//                       "|_  |_  |_|  _|   |   | |_| |_| |_  ", /* */
+//                       "|_| |_|   |  _|   |   |   |  _|  _| ", /* */
+//                       "                                    "};
+//     vector<string> ocr = Ocr::parse(lines);
+//     EXPECT_THAT(ocr, testing::ElementsAre("664371495 ERR"));
+// }
