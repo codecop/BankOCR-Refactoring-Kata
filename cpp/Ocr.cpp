43e23f0b009f6186719813e8b48b45d7f6e35eda
Peter Kofler <peter.kofler@code-cop.org>
2022-01-24 22:52:23 +0100

    Port code, but SegFault :-(

--- cpp/Ocr.cpp
+++ cpp/Ocr.cpp
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #include "Ocr.h"
 
 using namespace std;
@@ -45,34 +47,32 @@ const char Ocr::NUMERALS[10][4][5] = {{" _  ",  /* */
 
 vector<string> Ocr::parse(string lines[])
 {
-    // final List<String> result = new ArrayList<String>();
-    // for (int i = 0; i < lines.length; i += 4) {
-    //     final char[] work = "             ".toCharArray();
-    //     for (int pos = 0; pos < 9; ++pos) {
-    //         work[pos] = '?';
-    //         boolean got1 = false;
-    //         for (int numeral = 0; numeral <= 9; ++numeral) {
-    //             boolean ok = true;
-    //             for (int row = 0; row < 4; ++row) {
-    //                 for (int col = 0; col < 4; ++col) {
-    //                     if (NUMERALS[numeral][row][col] != lines[i + row].charAt(4 * pos + col))
-    //                         ok = false;
-    //                 }
-    //             }
-    //             if (ok) {
-    //                 work[pos] = (char)(numeral + (int)'0');
-    //                 got1 = true;
-    //                 break;
-    //             }
-    //         }
-    //         if (!got1) {
-    //             work[10] = 'I';
-    //             work[11] = work[12] = 'L';
-    //         }
-    //     }
-    //     result.add(new String(work));
-    // }
-    // return result;
-
-    return vector<string>{"888888888    "};
+    vector<string> result = {};
+    for (int i = 0; i < lines->size(); i += 4) {
+        char work[] = "             ";
+        for (int pos = 0; pos < 9; ++pos) {
+            work[pos] = '?';
+            bool got1 = false;
+            for (int numeral = 0; numeral <= 9; ++numeral) {
+                bool ok = true;
+                for (int row = 0; row < 4; ++row) {
+                    for (int col = 0; col < 4; ++col) {
+                        if (NUMERALS[numeral][row][col] != lines[i + row][4 * pos + col])
+                            ok = false;
+                    }
+                }
+                if (ok) {
+                    work[pos] = (char)(numeral + (int)'0');
+                    got1 = true;
+                    break;
+                }
+            }
+            if (!got1) {
+                work[10] = 'I';
+                work[11] = work[12] = 'L';
+            }
+        }
+        result.push_back(work);
+    }
+    return result;
 }
