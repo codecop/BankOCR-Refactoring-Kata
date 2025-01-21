33b48f794a4a8712f6460c8b335d7b9fec314d2e
Peter Kofler <peter.kofler@code-cop.org>
2025-01-21 21:51:04 +0100

    BacnkOCR pure ES6 version.

---
+++ javascript/src/ocr.js
@@ -0,0 +1,84 @@
+const NUMERALS = [
+    [' _  ',
+     '| | ',
+     '|_| ',
+     '    '],
+    ['    ',
+     '  | ',
+     '  | ',
+     '    '],
+    [' _  ',
+     ' _| ',
+     '|_  ',
+     '    '],
+    [' _  ',
+     ' _| ',
+     ' _| ',
+     '    '],
+    ['    ',
+     '|_| ',
+     '  | ',
+     '    '],
+    [' _  ',
+     '|_  ',
+     ' _| ',
+     '    '],
+    [' _  ',
+     '|_  ',
+     '|_| ',
+     '    '],
+    [' _  ',
+     '  | ',
+     '  | ',
+     '    '],
+    [' _  ',
+     '|_| ',
+     '|_| ',
+     '    '],
+    [' _  ',
+     '|_| ',
+     ' _| ',
+     '    ']];
+
+class Ocr {
+
+    /**
+     * @param {string[]} lines 
+     * @returns {string[]}
+     */
+    static parse(lines) {
+        /** @type {string[]} */
+        const result = [];
+        for (let i = 0; i < lines.length; i += 4) {
+            let work = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+            for (let pos = 0; pos < 9; ++pos) {
+                work[pos] = '?';
+                let got1 = false;
+                for (let numeral = 0; numeral <= 9; ++numeral) {
+                    let ok = true;
+                    for (let row = 0; row < 4; ++row) {
+                        for (let col = 0; col < 4; ++col) {
+                            if (NUMERALS[numeral][row][col] !== lines[i + row][4 * pos + col])
+                                ok = false;
+                        }
+                    }
+                    if (ok) {
+                        work[pos] = String.fromCharCode(numeral + '0'.charCodeAt(0));
+                        got1 = true;
+                        break;
+                    }
+                }
+                if (!got1) {
+                    work[10] = 'I';
+                    work[11] = work[12] = 'L';
+                }
+            }
+            result.push(work.join(''));
+        }
+        return result;
+    }
+}
+
+module.exports = {
+    Ocr
+};
